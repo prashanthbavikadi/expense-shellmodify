6671328847c9f478829d9e0ecf0db8099cf46522
bavikadi <Prashanthbavikadiias.com>
2025-03-20 08:55:23 +0530

    some-message

--- mysql-shell.sh
+++ mysql-shell.sh
@@ -8,17 +8,17 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>LOGFILE
-VALIDATE $? "Installing MYSQL server"
+dnf install mysql-serdever -y &>>LOGFILE
+
 
 systemctl enable mysqld  &>>LOGFILE
-VALIDATE $? "Enabling the MYSQL server"
+
  
 systemctl start mysqld &>>LOGFILE
-VALIDATE $?  "Starting  the MYSQL server"
+
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
@@ -28,7 +28,7 @@ mysql -h 172.31.93.44 -uroot -p${mysql_root_password} -e 'show databases;' &>>$L
 if [ $? -ne 0 ]
 then 
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MYSQL  Root Password setup"
+    
 else 
     echo -e "MYSQL root password is already setup ...$Y SKIP $N"
 fi         

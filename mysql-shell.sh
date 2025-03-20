d44f77df6990b01e197af4ea604d5115904b3e0b
bavikadi <Prashanthbavikadiias.com>
2025-03-20 08:26:54 +0530

    some-message

--- mysql-shell.sh
+++ mysql-shell.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 
-USERID=$(id -u )
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+check_root() # to call from one scrit from another acript we should keep in function i.e common.sh
+
 echo "Please enter DB password:"
 read -s mysql_root_password
-
-VALIDATE() {
-    if [ $? -ne 0 ]
-then
-    echo -e "$2  $R FAILURE"
-    exit1
-else 
-    echo -e .."$2  $G SUCCESS"
-fi
-
-}
-
-if [ $USERID -ne 0 ]
-then 
-    echo "please the run the root access"
-    exit2
-else
-    echo "you are in root access"
-fi
 
 dnf install mysql-server -y &>>LOGFILE
 VALIDATE $? "Installing MYSQL server"
